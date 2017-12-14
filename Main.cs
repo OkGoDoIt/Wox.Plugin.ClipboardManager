917d6fb292c9b590adc7feb75ad10d17a0e3532c
Roger Pincombe <roger.pincombe@capitalone.com>
2017-12-14 13:26:00 -0800

    fix * action keyword, newlines, weights, pasting

--- Main.cs
+++ Main.cs
@@ -8,78 +8,82 @@ using WindowsInput.Native;
 
 namespace Wox.Plugin.Clipboard
 {
-    public class Main : IPlugin
-    {
-        private const int MaxDataCount = 300;
-        private readonly KeyboardSimulator keyboardSimulator = new KeyboardSimulator(new InputSimulator());
-        private PluginInitContext context;
-        List<string> dataList = new List<string>();
+	public class Main : IPlugin
+	{
+		private const int MaxDataCount = 300;
+		private readonly KeyboardSimulator keyboardSimulator = new KeyboardSimulator(new InputSimulator());
+		private PluginInitContext context;
+		List<string> dataList = new List<string>();
 
-        public List<Result> Query(Query query)
-        {
-            var results = new List<Result>();
-            List<string> displayData;
-            if (query.ActionParameters.Count == 0)
-            {
-                displayData = dataList;
-            }
-            else
-            {
-                displayData = dataList.Where(i => i.ToLower().Contains(query.GetAllRemainingParameter().ToLower()))
-                        .ToList();
-            }
+		public List<Result> Query(Query query)
+		{
+			var results = new List<Result>();
+			List<string> displayData;
+			if (query.ActionParameters.Count == 0)
+			{
+				displayData = dataList;
+			}
+			else
+			{
+				displayData = dataList.Where(i => i.ToLower().Contains(query.Search.ToLower()))
+						.ToList();
+			}
 
-            results.AddRange(displayData.Select(o => new Result
-            {
-                Title = o.Trim(),
-                IcoPath = "Images\\clipboard.png",
-                Action = c =>
-                {
-                    try
-                    {
-                        System.Windows.Forms.Clipboard.SetText(o);
-                        context.API.HideApp();
-                        keyboardSimulator.ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.VK_V);
-                        return true;
-                    }
-                    catch (Exception e)
-                    {
-                        context.API.ShowMsg("Error", e.Message, null);
-                        return false;
-                    }
-                }
-            }).Reverse());
-            return results;
-        }
+			results.AddRange(displayData.Select((o, i) => new Result
+			{
+				Title = o.Replace(Environment.NewLine, " ").Trim(),
+				IcoPath = "Images\\clipboard.png",
+				Score = 30 + (query.Search.Length * query.Search.Length / o.Trim().Length) - (dataList.Count - i),
+				Action = c =>
+				{
+					try
+					{
+						System.Windows.Forms.Clipboard.SetText(o);
+						context.API.HideApp();
+						System.Threading.Timer tmr = new System.Threading.Timer(new System.Threading.TimerCallback(state =>
+								keyboardSimulator.ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.VK_V)
+							), null, 10, System.Threading.Timeout.Infinite);
 
-        public void Init(PluginInitContext context)
-        {
-            this.context = context;
-            ClipboardMonitor.OnClipboardChange += ClipboardMonitor_OnClipboardChange;
-            ClipboardMonitor.Start();
-        }
+						return true;
+					}
+					catch (Exception e)
+					{
+						context.API.ShowMsg("Error", e.Message, null);
+						return false;
+					}
+				}
+			}).Reverse());
+			return results;
+		}
 
-        void ClipboardMonitor_OnClipboardChange(ClipboardFormat format, object data)
-        {
-            if (format == ClipboardFormat.Html ||
-                format == ClipboardFormat.SymbolicLink ||
-                format == ClipboardFormat.Text ||
-                format == ClipboardFormat.UnicodeText)
-            {
-                if (data != null && !string.IsNullOrEmpty(data.ToString().Trim()))
-                {
-                    if (dataList.Contains(data.ToString()))
-                    {
-                        dataList.Remove(data.ToString());
-                    }
-                    dataList.Add(data.ToString());
+		public void Init(PluginInitContext context)
+		{
+			this.context = context;
+			ClipboardMonitor.OnClipboardChange += ClipboardMonitor_OnClipboardChange;
+			ClipboardMonitor.Start();
+		}
 
-                    if (dataList.Count > MaxDataCount)
-                    {
-                        dataList.Remove(dataList.Last());
-                    }
-                }
-            }
-        }
-    }
+		void ClipboardMonitor_OnClipboardChange(ClipboardFormat format, object data)
+		{
+			if (format == ClipboardFormat.Html ||
+				format == ClipboardFormat.SymbolicLink ||
+				format == ClipboardFormat.Text ||
+				format == ClipboardFormat.UnicodeText)
+			{
+				if (data != null && !string.IsNullOrEmpty(data.ToString().Trim()))
+				{
+					if (dataList.Contains(data.ToString()))
+					{
+						dataList.Remove(data.ToString());
+					}
+					dataList.Add(data.ToString());
+
+					if (dataList.Count > MaxDataCount)
+					{
+						dataList.Remove(dataList.Last());
+					}
+				}
+			}
+		}
+	}
 }
